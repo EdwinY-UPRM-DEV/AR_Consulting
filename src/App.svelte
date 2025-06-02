9c4a62cd43cf9af503c7e0c0151a179fc03c62c2
Edwin Y <edwin.almodovar3@upr.edu>
2025-06-01 19:47:36 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  
+</script>
+
+<main>
+  <div class="relative">
+  <button type="button" class="inline-flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900" aria-expanded="false">
+    <span>Solutions</span>
+    <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+      <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+    </svg>
+  </button>
+  </div>
+
+</main>
+
