b65164c5e095fcb3821ebe19ee9e51e70dbcbe91
Edwin Y <edwin.almodovar3@upr.edu>
2025-06-12 00:37:20 -0400

    header, responsive nav bar and menu cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,31 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import plusIcon from '/plus.svg'
   import menuBar from '/burger.svg'
   import companyLogo from '/A_R.png'
-  import Counter from './lib/Counter.svelte'
+
 
   let isOpen = false;
+  let isHovered = false;
 
   function toggleMenu() {
     isOpen = !isOpen;
   }
+
+  const handleEnter = () => isHovered = true;
+  const handleLeave = () => isHovered = false;
+
 </script>
 
-<main class='min-h-screen bg-gray-900'>
-  <nav class='relative bg-gradient-to-b from-blue-900 to-neutral-600 h-25 w-screen flex items-center justify-between px-7 border-blue-950 border-1 text-xl font-bold z-3 '>
-
+<main class='min-h-screen bg-gray-900 overflow-hidden'>
+  <nav id='topNav'class='relative bg-gradient-to-b from-blue-900 to-neutral-600 h-25 w-screen flex items-center justify-between px-7 border-blue-950 border-1 text-xl font-bold z-3 '>
     <div>
     <button
       type="button"
       on:click={toggleMenu}
       class='flex items-center justify-center text-white text-center outline-gray-600 hover:outline-2 bg-gray-650 bg-clip-border outline-offset-4 rounded focus:bg-gray-600 focus:outline-2' >
-      <img class=' invert w-6 h-6 mr-2' src={menuBar}>
+      <img class='invert w-6 h-6 mr-2' src={menuBar}>
       <span>Explore</span>
     </button>
     </div>
-    
-
 
     <div class='absolute left-1/2 transform -translate-x-1/2 flex justify-center items-center'>
     <img class='w-16 h-16' src={companyLogo} alt='logo'>
@@ -40,13 +39,62 @@
         <span>Schedule Now</span>
         </button>
     </div>
-
   </nav>
-  <nav class='relative h-10 w-2/3 mx-auto bottom-3 flex justify-center items-center bg-slate-700 rounded-b-3xl shadow-xl/20 z-2 hover:bottom-0 hover:h-30'>
-    <div>
-    </div>
-    <div>
+  
+
+  <nav id='fullMenu' class='relative h-60 w-2/3 mx-auto bottom-50 flex justify-center items-start bg-slate-700 rounded-b-3xl shadow-xl/20 z-2 transition delay-150 ease-in-out duration-300 {isHovered ? 'translate-y-50' : 'translate-y-0'}'>
+
+    
+    <div 
+    id="hoverZone" 
+    role="button"
+    aria-label="Open Nav"
+    tabindex="0"
+    on:mouseenter={handleEnter}
+    on:mouseleave={handleLeave}
+    class="absolute top-0 left-1/100 h-60 w-[15%] z-30 {isHovered ? 'w-full h-70' : 'w-[15%] h-60'}">
+  </div>  
+
+
+
+  
+  <div id='menuCards' class='flex absolute min-w-full min-h-full'>
+    <div class='flex justify-center space-x-20 min-w-full min-h-full'>
+
+    <div class="flex relative flex-col top-10 bg-white rounded-2xl shadow-xl max-w-120 max-h-30 p-5 space-y-4 justify-center items-center">      
+      <div class='flex space-x-60'>
+      <h2 class="text-s font-bold">Card Title</h2>
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+      </svg>
       </div>
+      <div class=" flex text-gray-600">This card has an image, title, text, and a button.</div>
+    </div>
+    
+    <div class="flex relative flex-col top-10 bg-white rounded-2xl shadow-xl max-w-120 max-h-30 p-5 space-y-4 justify-center items-center">      
+      <div class='flex space-x-60'>
+        <h2 class="text-s font-bold">Card Title 2</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+        </svg>
+      </div>
+      <div class="flex text-gray-600">This card has an image, title, text, and a button.
+      </div>
+    </div>
+  </div>
+  
+</div>
 
+  <div id='menuSelection'>
+      <ul class='relative flex text-s text-white space-x-5 sm:space-x-10 md:space-x-20 lg:space-x-35 mt-52'>
+      <li><a href="#" class=" hover:text-blue-600">Home</a></li>
+      <li><a href="#" class=" hover:text-blue-600">About</a></li>
+      <li><a href="#" class=" hover:text-blue-600">Services</a></li>
+      <li><a href="#" class=" hover:text-blue-600">Overview</a></li>
+    </ul>
+    </div>
   </nav>
+
+
+
 </main>
---
+++ src/work.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import menuBar from '/burger.svg'
+  import companyLogo from '/A_R.png'
+
+
+  let isOpen = false;
+  let isHovered = false;
+
+  function toggleMenu() {
+    isOpen = !isOpen;
+  }
+
+  const handleEnter = () => isHovered = true;
+  const handleLeave = () => isHovered = false;
+
+</script>
+
+<div class="absolute flex-col top-10 bg-white rounded-2xl shadow-xl max-w-120 max-h-30 p-5 space-y-4 justify-center items-center">      
+      <div class='flex space-x-60'>
+      <h2 class="text-s font-bold">Card Title</h2>
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+      </svg>
+      </div>
+      <div class=" flex text-gray-600">This card has an image, title, text, and a button.</div>
+
